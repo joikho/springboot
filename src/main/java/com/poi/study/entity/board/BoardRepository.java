ab6da2eb5a18f2b4b0085b775f918a24dc27846c
joikho <it890122@naver.com>
2023-03-04 16:09:00 +0900

    board view add

--- src/main/java/com/poi/study/entity/board/BoardRepository.java
+++ src/main/java/com/poi/study/entity/board/BoardRepository.java
@@ -1,21 +1,31 @@
 package com.poi.study.entity.board;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import com.poi.study.dto.board.BoardRequestDto;
 
-public class BoardRepository extends JpaRepository <Board, Long>{
+public interface BoardRepository extends JpaRepository <Board, Long>{
 
 	String UPDATE_BOARD = "UPDATE Board"+
-	"SET TITLE =:#{#boardRequestDto.title}, "+
-	"CONTNET =:#{#boardRequestDto.content}, "+
-	"UPDATE_TIME = NOW() "+
-	"WHERE ID = :#{#boardRequestDto.id}";
+			"SET TITLE =:#{#boardRequestDto.title}, "+
+			"CONTNET =:#{#boardRequestDto.content}, "+
+			"UPDATE_TIME = NOW() "+
+			"WHERE ID = :#{#boardRequestDto.id}";
 	
+	static final String UPDATE_BOARD_READ_CNT_INC = "UPDATE Board "
+			+ "SET READ_CNT = READ_CNT + 1 "
+			+ "WHERE ID = :id";
 	
 	@Transactional
 	@Modifying
 	@Query(value = UPDATE_BOARD, nativeQuery = true)
-	public int updateBoard(@Param("boardRequestDto") BoardRequestDto boardRequestDto)
+	public int updateBoard(@Param("boardRequestDto") BoardRequestDto boardRequestDto);
+
+	@Transactional
+	@Modifying
+	@Query(value = UPDATE_BOARD_READ_CNT_INC, nativeQuery = true)
+	public int updateBoardReadCntInc(@Param("id") Long id);
 }
--- src/test/java/com/poi/study/BoardTestApplicationTests.java
+++ src/test/java/com/poi/study/BoardTestApplicationTests.java
@@ -1,13 +1,76 @@
 package com.poi.study;
+	
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.poi.study.dto.board.BoardRequestDto;
+import com.poi.study.dto.board.BoardResponseDto;
+import com.poi.study.service.BoardService;
 
 @SpringBootTest
 class BoardTestApplicationTests {
-
+	
+	@Autowired
+	private BoardService boardService;
+	
 	@Test
-	void contextLoads() {
+	void save() {
+		BoardRequestDto boardSaveDto = new BoardRequestDto();
+		
+		boardSaveDto.setTitle("제목입니다.");
+		boardSaveDto.setContent("내용입니다.");
+		boardSaveDto.setRegisterId("작성자");
+		
+		Long result = boardService.save(boardSaveDto);
+		
+		if(result >0) {
+			System.out.println("# success save() ~");
+			findAll();
+			findById(result);
+		}else {
+			System.out.println("Fail Save() ~");
+			
+		}
 	}
-
+	
+	void findAll() {
+		List < BoardResponseDto > list = boardService.findAll();
+		
+		if(list != null) {
+			System.out.println("# success findAll() : "+ list.toString());
+		}else {
+			System.out.println("# Fail findAll() ~");
+		}
+	}
+	
+	void findById(Long id) {
+		BoardResponseDto info = boardService.findById(id);
+		
+		if(info != null) {
+			System.out.println("# Success findById() :"+ info.toString());
+		}else {
+			System.out.println("# Fail findById() ~");
+		}
+	}
+	
+	void updateBoard(Long id) {
+		
+		BoardRequestDto boardRequestDto = new BoardRequestDto();
+		
+		boardRequestDto.setId(id);
+		boardRequestDto.setTitle("업데이트 제목");
+		boardRequestDto.setContent("업데이트 내용");
+		boardRequestDto.setRegisterId("작성자");
+		
+		int result = boardService.updateBoard(boardRequestDto);
+		
+		if(result > 0) {
+			System.out.println("# successs updateBoard() ~");
+		} else {
+			System.out.println("# Fail updateBoard() ~");
+		}
+	}
 }
